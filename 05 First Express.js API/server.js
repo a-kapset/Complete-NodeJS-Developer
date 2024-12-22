6f7b6aeaed7acf75476e2ee0daf5c9f0f5c613c4
Artsiom Kapset <artsiom.kapset@mmc.com>
2024-12-22 12:54:38 +0100

    middlewaresare added

--- 05 First Express.js API/server.js
+++ 05 First Express.js API/server.js
@@ -17,6 +17,27 @@ const friends = [
   },
 ]
 
+app.use((req, res, next) => {
+  const start = Date.now()
+  next()
+  const delta = Date.now() - start
+  console.log(`${req.method} ${req.url} ${delta} ms`)
+})
+
+app.use(express.json())
+
+app.post('/friends', (req, res) => {
+  if (!req.body.name) return res.status(400).json({ error: 'Name is missed' })
+
+  const newFriend = {
+    id: friends.length,
+    name: req.body.name,
+  }
+
+  friends.push(newFriend)
+  res.json(newFriend)
+})
+
 app.get('/friends', (req, res) => {
   res.json(friends)
 })
@@ -36,6 +57,4 @@ app.post('/messages', (req, res) => {
   console.log('Updating messages...')
 })
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
