edf58e1afac8db1c5de1fe6b806bcd143cacc013
Artsiom Kapset <artsiom.kapset@mmc.com>
2024-12-22 20:42:57 +0100

    file sending is added

--- 05 First Express.js API/controllers/messages.controller.js
+++ 05 First Express.js API/controllers/messages.controller.js
@@ -1,5 +1,7 @@
+const path = require('path')
+
 function getMessages(req, res) {
-  res.send('<ul><li>Hello_1</li><li>Hello_2</li></ul>')
+  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'))
 }
 
 function postMessage(req, res) {
