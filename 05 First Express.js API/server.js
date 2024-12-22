7928b2c84c5b09da09d31707979339115211f9ea
Artsiom Kapset <artsiom.kapset@mmc.com>
2024-12-22 11:51:06 +0100

    nodemon is added

--- 05 First Express.js API/server.js
+++ 05 First Express.js API/server.js
@@ -25,7 +25,7 @@ app.get('/friends/:friendId', (req, res) => {
   const friendId = Number(req.params.friendId)
   const friend = friends[friendId]
   if (friend) res.status(200).json(friend)
-  else res.status(404).json({ error: 'Friend does not exist' })
+  else res.status(404).json({ error: 'Friend does not exist...' })
 })
 
 app.get('/messages', (req, res) => {
