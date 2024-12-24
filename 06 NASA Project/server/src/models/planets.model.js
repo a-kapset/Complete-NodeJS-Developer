e172c1dd4a1e9a9ecfb42b889dd974ad7f5e3fd8
Artsiom Kapset <artsiom.kapset@mmc.com>
2024-12-24 14:55:22 +0100

    mvc is upadated

--- 06 NASA Project/server/src/models/planets.model.js
+++ 06 NASA Project/server/src/models/planets.model.js
@@ -36,7 +36,11 @@ function loadPlanetData() {
   })
 }
 
+function getAllPlanets() {
+  return habitablePlanets
+}
+
 module.exports = {
   loadPlanetData,
-  planets: habitablePlanets,
+  getAllPlanets,
 }
