472ffb333a098d3d3e2b4cf12edd85297346ac63
Artsiom Kapset <artsiom.kapset@ventionteams.com>
2025-01-07 17:42:40 +0100

    pagination is added

--- 06 NASA Project/server/src/models/launches.model.js
+++ 06 NASA Project/server/src/models/launches.model.js
@@ -5,19 +5,6 @@ const planets = require('./planets.mongo');
 const DEFAULT_FLIGHT_NUMBER = 100;
 const SPACEX_URL_API = 'https://api.spacexdata.com/v4/launches/query';
 
-const launch = {
-  flightNumber: 100,
-  mission: 'Kepler Exploration X',
-  rocket: 'Explorer IS1',
-  launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442 b',
-  customers: ['ZTM', 'NASA'],
-  upcoming: true,
-  success: true,
-};
-
-saveLaunch(launch);
-
 async function populateLaunches() {
   console.log('Loading launch data');
 
@@ -88,8 +75,12 @@ async function getLatestFlightNumber() {
   return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches() {
-  return await launches.find({}, { __v: 0, _id: 0 }); // __v & _id are excluded from db response
+async function getAllLaunches(skip, limit) {
+  return await launches
+    .find({}, { __v: 0, _id: 0 }) // __v & _id are excluded from db response
+    .sort({ flightNumber: 1 })
+    .skip(skip)
+    .limit(limit);
 }
 
 async function saveLaunch(launch) {
--- 06 NASA Project/server/src/routes/launches/launches.controller.js
+++ 06 NASA Project/server/src/routes/launches/launches.controller.js
@@ -1,7 +1,10 @@
 const launchesModel = require('../../models/launches.model');
+const { getPagination } = require('../../services/query');
 
 async function httpGetAllLaunches(req, res) {
-  return res.status(200).json(await launchesModel.getAllLaunches());
+  const { skip, limit } = getPagination(req.query);
+  const launches = await launchesModel.getAllLaunches(skip, limit);
+  return res.status(200).json(launches);
 }
 
 async function httpAddNewLaunch(req, res) {
