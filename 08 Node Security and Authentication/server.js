dce35a9c0bcd5c50f8b1752b99c7286eab0e3ee0
Artsiom Kapset <artsiom.kapset@ventionteams.com>
2025-01-08 18:09:35 +0100

    security example is added

---
+++ 08 Node Security and Authentication/server.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/secret', (req, res) => {
+  return res.send('Secret data');
+});
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+);
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
+  });
